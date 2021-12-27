8701b0dabf6d7137edf5006af3b26c8536824b46
Nicolas <pnwa@icloud.com>
2021-12-27 16:46:52 +0100

    Add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const { now } = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    publicationDate: {
+        type: Date,
+        required: true,
+        default: now()
+    },
+    authorId: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    postId: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+})
+
+module.exports = mongoose.model('Comment', commentSchema)
