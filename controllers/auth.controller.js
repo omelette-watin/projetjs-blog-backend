029d9717ed463f4e8eaec634bcc11d6568b1eb18
Nicolas <pnwa@icloud.com>
2021-12-29 23:02:10 +0100

    Add field verification for sign-up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,70 +4,90 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const SECRET_KEY = process.env.SECRET_KEY
 
-async function userExistByEmail(email) {
+const userExistByEmail= async (email) => {
     return User.findOne({email})
 }
 
-async function userExistByUsername(username) {
+const userExistByUsername = async (username) => {
     return User.findOne({username})
 }
 
 exports.signUp = async (req, res) => {
-    const { username, email, password, role } = req.body
+    try {
+        const {username, email, password, role} = req.body
 
-    if (await userExistByEmail(email)) return res.status(401).json({
-        message: "Email is already used"
-    })
+        if (!email || !username || !password) return res.status(400).json({
+            message: "All fields are required"
+        })
 
-    if (await userExistByUsername(username)) return res.status(401).json({
-        message: "Username is already used"
-    })
+        if (!email.includes("@")) return res.status(400).json({
+            message: "Email is invalid"
+        })
 
-    const newUser = new User({
-        username,
-        email,
-        role,
-        password: await User.hashPassword(password)
-    })
+        if (await userExistByEmail(email)) return res.status(401).json({
+            message: "Email already used"
+        })
 
-    const savedUser = await newUser.save()
+        if (await userExistByUsername(username)) return res.status(401).json({
+            message: "Username already used"
+        })
 
-    const token = jwt.sign(
-        { id: savedUser._id },
-        SECRET_KEY,
-        { expiresIn: 604800 } // 1 week
-    )
+        const newUser = new User({
+            username,
+            email,
+            role,
+            password: await User.hashPassword(password)
+        })
 
-    res.status(200).json({
-        message: "User successfuly created",
-        token
-    })
+        const savedUser = await newUser.save()
+
+        const token = jwt.sign(
+            {id: savedUser._id},
+            SECRET_KEY,
+            {expiresIn: 604800} // 1 week
+        )
+
+        res.status(200).json({
+            message: "User successfuly created",
+            token
+        })
+    }  catch (err) {
+        res.status(500).json({
+            message: err.message || "Something went wrong, please try later"
+        })
+    }
 }
 
 exports.logIn = async (req, res) => {
-    const { email, password, username } = req.body
+    try {
+        const {email, password, username} = req.body
 
-    const userExist = await userExistByEmail(email) ? await userExistByEmail(email) : await userExistByUsername(username)
+        const userExist = await userExistByEmail(email) ? await userExistByEmail(email) : await userExistByUsername(username)
 
-    if (!userExist) return res.status(401).json({
-        message: "Incorrect email or password"
-    })
+        if (!userExist) return res.status(401).json({
+            message: "Incorrect email or password"
+        })
 
-    const matchPassword = await User.comparePassword(password, userExist.password)
+        const matchPassword = await User.comparePassword(password, userExist.password)
 
-    if (!matchPassword) return res.status(401).json({
-        message: "Incorrect email or password"
-    })
-    
-    const token = jwt.sign(
-        { id: userExist._id },
-        SECRET_KEY,
-        { expiresIn: 604800 } // 1 week
-    )
-    
-    return res.status(200).json({
-        _id: userExist._id,
-        message: 'Successfuly logged in',
-        token: token,
-    })
+        if (!matchPassword) return res.status(401).json({
+            message: "Incorrect email or password"
+        })
+
+        const token = jwt.sign(
+            {id: userExist._id},
+            SECRET_KEY,
+            {expiresIn: 604800} // 1 week
+        )
+
+        return res.status(200).json({
+            _id: userExist._id,
+            message: 'Successfuly logged in',
+            token: token,
+        })
+    }  catch (err) {
+        res.status(500).json({
+            message: err.message || "Something went wrong, please try later"
+        })
+    }
 }
